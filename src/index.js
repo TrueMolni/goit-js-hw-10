cd1c79d2888afea5cc628bcad51ceff9542d7ca5
TrueMolni <truemolni@gmail.com>
2022-12-01 16:03:11 +0200

    Added debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
-
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  queryRef: document.querySelector('#search-box'),
+  listRef: document.querySelector('.country-list'),
+  infoRef: document.querySelector('.country-info'),
+};
+
+function fetchCountries(name) {
+  console.log('hello');
+}
+console.log(refs.queryRef);
+console.log(refs.listRef);
+console.log(refs.infoRef);
+
+const queryHandle = () => {
+  console.log(refs.queryRef.value);
+};
+
+refs.queryRef.addEventListener('input', debounce(queryHandle, DEBOUNCE_DELAY));
